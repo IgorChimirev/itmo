ab82b6b1035df2ee6fd072ae9c51f37277db9646
IgorChimirev <igor.chimirev@gmail.com>
2024-11-13 12:14:26 +0300

    dop3(2).py

--- инфа/lab4/tasks/dop3(2).py
+++ инфа/lab4/tasks/dop3(2).py
@@ -5,37 +5,43 @@
 <content> ::= <text> | <element> | <content> <element>
 <attributes> ::= <attribute> | <attribute> <attributes>
 <attribute> ::= <attr_name>="value"
-<tag_name> ::= "item" | "name" | "age"  // и так далее для всех возможных имен тегов
-<text> ::= "..."  // строка текста, оставляем для специфики
+<tag_name> ::= <string>  // любое имя тега
+<text> ::= <string>  // текстовое содержимое
+
 '''
 import json
 
 def json_to_xml(json_obj, line_padding=""):
-    """Конвертирует JSON в XML."""
-    xml_str = ""
-
     if isinstance(json_obj, dict):
-        for tag_name in json_obj:
+        xml_str = ""
+        for tag_name, value in json_obj.items():
+            # <start_tag> ::= <tag_name> | <tag_name> <attributes>
             xml_str += f"{line_padding}<{tag_name}>\n"
-            xml_str += json_to_xml(json_obj[tag_name], line_padding + "  ")
+            # <content> ::= <text> | <element> | <content> <element>
+            xml_str += json_to_xml(value, line_padding + "  ")
+            # <end_tag> ::= </<tag_name>>
             xml_str += f"{line_padding}</{tag_name}>\n"
+        return xml_str
     elif isinstance(json_obj, list):
-        for sub_elem in json_obj:
-            xml_str += json_to_xml(sub_elem, line_padding)
+        xml_str = ""
+        for item in json_obj:
+            xml_str += json_to_xml(item, line_padding)
+        return xml_str
     else:
-        xml_str += f"{line_padding}{json_obj}\n"
-
-    return xml_str
+        # <text> ::= <string>
+        return f"{line_padding}{json_obj}\n"
 
 # Чтение JSON из файла
-with open('q.json', 'r') as json_file:
+with open('input.json', 'r') as json_file:
     json_data = json.load(json_file)
 
 # Конвертация JSON в XML
 xml_result = json_to_xml(json_data)
 
 # Запись XML в файл
-with open('outdop3.xml', 'w') as xml_file:
+with open('output.xml', 'w') as xml_file:
     xml_file.write(xml_result)
 
 print("Конвертация завершена! XML сохранен в output.xml")
+
+")
