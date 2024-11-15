ec88a8d74b1db74ef649fec979326f16df9c4cdc
IgorChimirev <igor.chimirev@gmail.com>
2024-11-15 11:18:15 +0300

    Update dop1.py

--- инфа/lab4/tasks/dop1.py
+++ инфа/lab4/tasks/dop1.py
@@ -6,23 +6,24 @@ import time
 start_time = time.time()
 
 def json_to_xml(json_file, xml_file):
-    # Чтение JSON данных из файла
+
     with open(json_file, 'r') as f:
         data = json.load(f)
 
-    # Преобразование данных в XML
+
     xml_data = dicttoxml(data, custom_root='root', attr_type=False)
 
-    # Запись XML данных в файл
+
     with open(xml_file, 'wb') as f:
         f.write(xml_data)
 
 if __name__ == "__main__":
-    json_file = 'q.json'  # Замените на имя вашего JSON файла
-    xml_file = 'out.xml'     # Замените на желаемое имя XML файла
+    json_file = 'q.json' 
+    xml_file = 'outdop1.xml'     
     json_to_xml(json_file, xml_file)
 
 
 end_time = time.time()
-elapsed_time = end_time - start_time  # Исчисляем прошедшее время
-print(f"Время выполнения: {elapsed_time} секунд")
+el_time = end_time - start_time  
+print(f"Время выполнения: {el_time} секунд")
+
